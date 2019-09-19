7562567a1c99253e8a54570288290caa6b4291d0
ltzyp <ltzypunov@gmail.com>
2019-09-19 22:28:52 +0300

    add_context

--- app/models/context_node.rb
+++ app/models/context_node.rb
@@ -3,8 +3,11 @@ class ContextNode < ApplicationRecord
   belongs_to :parent_node, class_name: 'ContextNode', foreign_key: :parent_id, optional: true
 
   has_many  :context_nodes, foreign_key: :parent_id 
+  serialize :context
+
 
   after_initialize do
+    self.context||=  {} 
     copy_context_from_parent
   end
 
@@ -20,13 +23,18 @@ class ContextNode < ApplicationRecord
     return 0 unless parent_node
     return  parent_node.level + 1     
   end
+
+  def add_context hash
+    self.context.merge!( hash||{} )
+  end
+
 private
 
 
   def  copy_context_from_parent
     if parent_node
-      self.context_tree= parent_node.context_tree #; self
-      self.context= parent_node.context
+      self.context_tree= self.parent_node.context_tree #; self
+      self.context.merge!( parent_node.context||{})
     end
   end
 end
